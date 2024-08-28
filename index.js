42c70bc42b2ae4d3ca79a51385326b290bba705c
samirthaker2020 <samir2492@outlook.com>
2024-08-28 19:20:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require('./utils/swagger_output.json')
+const app = express();
+const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/users.route");
+const port = 3000;
+app.use(bodyParser.json());
+
+/** Swagger Initialization - START */
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+/** Swagger Initialization - END */
+
+app.use("/users", usersRoutes);
+
+app.listen(port, () => {
+  console.log("Server is running at port 3000");
+});
