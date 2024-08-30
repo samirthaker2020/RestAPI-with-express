dfd6b7b718a1037136e9c6209e30f9a2c0dbe115
samirthaker2020 <samir2492@outlook.com>
2024-08-30 00:53:41 -0300

    test: add posts test

--- src/test/unit-tests/posts.test.js
+++ src/test/unit-tests/posts.test.js
@@ -6,6 +6,6 @@ describe('user makes posts', () => {
   it('should return all posts', async () => {
     const res = await request(app).get('/posts/get-all-post');
     expect(res.status).to.equal(200);
-    // expect(res.body.message).to.equal('Hello, world!');
+    expect(res.body).to.be.an("object");
   });
 });
